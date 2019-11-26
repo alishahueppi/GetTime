6d14b00dd4b42c64db859b39f2adb69cccd43681
alisha <alisha.hueppi@edu.gbssg.ch>
2019-11-26 17:57:28 +0100

    feat: grundgerüst fertig ergänzt

--- GetTime/src/app/app.module.ts
+++ GetTime/src/app/app.module.ts
@@ -4,6 +4,7 @@ import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {
+    MatCardModule,
     MatDatepickerModule,
     MatFormFieldModule,
     MatIconModule,
@@ -30,6 +31,7 @@ import { CountableValuesComponent } from './countable-values/countable-values.co
         MatNativeDateModule,
         MatIconModule,
         MatInputModule,
+        MatCardModule,
     ],
     providers: [],
     bootstrap: [AppComponent]
